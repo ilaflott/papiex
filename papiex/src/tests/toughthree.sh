c0685cba226d05893c7e50a73e125e553d9ad92d
Ian L. <6273252+ilaflott@users.noreply.github.com>
2025-07-25 09:58:28 -0400

    Update toughthree.sh

--- papiex/src/tests/toughthree.sh
+++ papiex/src/tests/toughthree.sh
@@ -2,7 +2,10 @@
 unset PATH
 tcl=/usr/bin/tclsh8.5
 if [[ -x ${tcl} ]]; then
+    ${tcl} toughone.tcl
+    ${tcl} toughtwo.tcl
     ${tcl} toughthree.tcl
 else
+    echo "AM I SKIPPING BY EXITING 0 (FAUX SUCCESS) or AM I GOOD?"
     exit 0
 fi
